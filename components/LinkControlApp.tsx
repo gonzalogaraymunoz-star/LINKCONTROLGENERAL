bb94145a6de78f0ee56b0bb765f55ef17b4352f5
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-08-26 14:21:25 -0400

    feat: surface onboarding factory board

--- components/LinkControlApp.tsx
+++ components/LinkControlApp.tsx
@@ -3,11 +3,13 @@
 import ControlActionsButton from "./ControlActionsButton";
 import LinkControlAppV4 from "./LinkControlAppV4";
 import OperationalDock from "./OperationalDock";
+import ClientOnboardingBoard from "./ClientOnboardingBoard";
 
 export default function LinkControlApp(props: { initialScope?: string } = {}) {
   return (
     <>
       <LinkControlAppV4 {...props} />
+      <ClientOnboardingBoard />
       <OperationalDock />
       <div className="controlActionStandalone"><ControlActionsButton /></div>
     </>
