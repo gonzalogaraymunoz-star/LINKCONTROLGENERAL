f07cdea18d07988d483f31e436bb61e1bdced8d1
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-08-26 14:43:55 -0400

    feat: connect missions board to control central

--- components/LinkControlApp.tsx
+++ components/LinkControlApp.tsx
@@ -4,12 +4,14 @@ import ControlActionsButton from "./ControlActionsButton";
 import LinkControlAppV4 from "./LinkControlAppV4";
 import OperationalDock from "./OperationalDock";
 import ClientOnboardingBoard from "./ClientOnboardingBoard";
+import MissionsBoard from "./MissionsBoard";
 
 export default function LinkControlApp(props: { initialScope?: string } = {}) {
   return (
     <>
       <LinkControlAppV4 {...props} />
       <ClientOnboardingBoard />
+      <MissionsBoard />
       <OperationalDock />
       <div className="controlActionStandalone"><ControlActionsButton /></div>
     </>
