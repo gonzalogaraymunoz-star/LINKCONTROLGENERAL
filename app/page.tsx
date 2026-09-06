495dabd545128487aa833c668c3abcbbe3855157
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-06 19:58:26 -0300

    feat: expose Personal Mission below Integrations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import LinkControlApp from "@/components/LinkControlApp";
 import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
+import PersonalMissionMenuLink from "@/components/PersonalMissionMenuLink";
 
 export default function Home() {
   return (
     <>
       <LinkControlApp />
+      <PersonalMissionMenuLink />
       <Ficha360QuickAccess />
     </>
   );
