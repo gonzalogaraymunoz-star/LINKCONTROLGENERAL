048bc4482231198107cdacd49b0a331a377c627b
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-06 20:16:22 -0300

    cleanup: use native Personal Mission sidebar entry

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import LinkControlApp from "@/components/LinkControlApp";
 import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
-import PersonalMissionMenuLink from "@/components/PersonalMissionMenuLink";
 
 export default function Home() {
   return (
     <>
       <LinkControlApp />
-      <PersonalMissionMenuLink />
       <Ficha360QuickAccess />
     </>
   );
