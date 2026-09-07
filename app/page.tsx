2a96e4b2d696244b3d2354be5473a86f0e90c71b
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-07 15:00:16 -0300

    fix: mount navigation and calendar UX bridge

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
 import DashboardPersonalMissionHero from "@/components/DashboardPersonalMissionHero";
 import LinkThemeController from "@/components/LinkThemeController";
 import UnifiedWorkBoard from "@/components/UnifiedWorkBoard";
+import ControlCentralUXBridge from "@/components/ControlCentralUXBridge";
 
 export default function Home() {
   return (
     <>
       <LinkControlApp />
       <LinkThemeController />
+      <ControlCentralUXBridge />
       <DashboardPersonalMissionHero />
       <UnifiedWorkBoard />
       <Ficha360QuickAccess />
