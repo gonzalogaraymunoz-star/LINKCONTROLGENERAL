7a7ea6b8552fdc8cccc28a5871ad396d97bed0b0
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-06 22:59:47 -0300

    feat: mount LINK theme controller on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import LinkControlApp from "@/components/LinkControlApp";
 import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
 import DashboardPersonalMissionHero from "@/components/DashboardPersonalMissionHero";
+import LinkThemeController from "@/components/LinkThemeController";
 
 export default function Home() {
   return (
     <>
       <LinkControlApp />
+      <LinkThemeController />
       <DashboardPersonalMissionHero />
       <Ficha360QuickAccess />
     </>
