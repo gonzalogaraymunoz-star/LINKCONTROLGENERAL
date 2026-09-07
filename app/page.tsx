7e16cc239dce6cd734c98b06ea9d075b001f3545
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-06 22:36:31 -0300

    feat: mount Personal Mission hero on Control Central home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import LinkControlApp from "@/components/LinkControlApp";
 import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
+import DashboardPersonalMissionHero from "@/components/DashboardPersonalMissionHero";
 
 export default function Home() {
   return (
     <>
       <LinkControlApp />
+      <DashboardPersonalMissionHero />
       <Ficha360QuickAccess />
     </>
   );
