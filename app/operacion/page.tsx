12fef2cfd44d82825527274829d235856dc6d034
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-24 23:25:37 -0300

    refactor: reduce personal mission from operational workspace

--- app/operacion/page.tsx
+++ app/operacion/page.tsx
@@ -1,13 +1,11 @@
 import LinkControlApp from "@/components/LinkControlApp";
 import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
-import DashboardPersonalMissionHero from "@/components/DashboardPersonalMissionHero";
 import LinkThemeController from "@/components/LinkThemeController";
-import UnifiedWorkBoard from "@/components/UnifiedWorkBoard";
 import ControlCentralUXBridge from "@/components/ControlCentralUXBridge";
 import ActivityHumanizer from "@/components/ActivityHumanizer";
 import TaskActionBridge from "@/components/TaskActionBridge";
 
-export default function Home() {
+export default function OperationHome() {
   return (
     <>
       <LinkControlApp />
@@ -15,8 +13,6 @@ export default function Home() {
       <ControlCentralUXBridge />
       <ActivityHumanizer />
       <TaskActionBridge />
-      <DashboardPersonalMissionHero />
-      <UnifiedWorkBoard />
       <Ficha360QuickAccess />
     </>
   );
