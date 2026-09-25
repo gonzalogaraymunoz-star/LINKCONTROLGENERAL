252cc14be6a0896e7b2774b0b62450e442590ab4
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-24 23:03:49 -0300

    feat: make Link World dashboard the Control Central home

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import LinkControlApp from "@/components/LinkControlApp";
-import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
-import DashboardPersonalMissionHero from "@/components/DashboardPersonalMissionHero";
-import LinkThemeController from "@/components/LinkThemeController";
-import UnifiedWorkBoard from "@/components/UnifiedWorkBoard";
-import ControlCentralUXBridge from "@/components/ControlCentralUXBridge";
-import ActivityHumanizer from "@/components/ActivityHumanizer";
-import TaskActionBridge from "@/components/TaskActionBridge";
+import LinkWorldDashboard from "@/components/LinkWorldDashboard";
 
 export default function Home() {
-  return (
-    <>
-      <LinkControlApp />
-      <LinkThemeController />
-      <ControlCentralUXBridge />
-      <ActivityHumanizer />
-      <TaskActionBridge />
-      <DashboardPersonalMissionHero />
-      <UnifiedWorkBoard />
-      <Ficha360QuickAccess />
-    </>
-  );
+  return <LinkWorldDashboard />;
 }
