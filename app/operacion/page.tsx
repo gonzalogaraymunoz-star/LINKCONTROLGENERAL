5d2fd6cc6ef4693c527e40a30526cfca993bca1e
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-24 23:37:32 -0300

    feat: replace operation dashboard with gesture task inbox

--- app/operacion/page.tsx
+++ app/operacion/page.tsx
@@ -1,19 +1,5 @@
-import LinkControlApp from "@/components/LinkControlApp";
-import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
-import LinkThemeController from "@/components/LinkThemeController";
-import ControlCentralUXBridge from "@/components/ControlCentralUXBridge";
-import ActivityHumanizer from "@/components/ActivityHumanizer";
-import TaskActionBridge from "@/components/TaskActionBridge";
+import GestureTaskApp from "@/components/GestureTaskApp";
 
 export default function OperationHome() {
-  return (
-    <>
-      <LinkControlApp />
-      <LinkThemeController />
-      <ControlCentralUXBridge />
-      <ActivityHumanizer />
-      <TaskActionBridge />
-      <Ficha360QuickAccess />
-    </>
-  );
+  return <GestureTaskApp />;
 }
