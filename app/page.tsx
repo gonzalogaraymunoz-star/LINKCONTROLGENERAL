4558dd98e56f22842d246549a62b83f39140cc77
gonzalogaraymunoz-star <gonzalogaraymunoz@gmail.com>
2026-09-25 16:20:12 -0300

    Replace legacy Control Central home with current LINK WORLD

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import LinkControlApp from "@/components/LinkControlApp";
-import Ficha360QuickAccess from "@/components/Ficha360QuickAccess";
-import DashboardPersonalMissionHero from "@/components/DashboardPersonalMissionHero";
-import LinkThemeController from "@/components/LinkThemeController";
-import UnifiedWorkBoard from "@/components/UnifiedWorkBoard";
-import ControlCentralUXBridge from "@/components/ControlCentralUXBridge";
-import ActivityHumanizer from "@/components/ActivityHumanizer";
-import TaskActionBridge from "@/components/TaskActionBridge";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <LinkControlApp />
-      <LinkThemeController />
-      <ControlCentralUXBridge />
-      <ActivityHumanizer />
-      <TaskActionBridge />
-      <DashboardPersonalMissionHero />
-      <UnifiedWorkBoard />
-      <Ficha360QuickAccess />
-    </>
-  );
+  redirect("https://link-world-delta.vercel.app");
 }
